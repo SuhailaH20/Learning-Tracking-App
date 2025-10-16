5733d4c9084b208b6cb4523d727c8a8718423318
Suhaylah <suhailahawsawi@gmail.com>
2025-10-16 12:49:38 +0300

    the first icon

--- Learning Tracking App/ContentView.swift
+++ Learning Tracking App/ContentView.swift
@@ -10,13 +10,23 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ZStack {
+                RoundedRectangle(cornerRadius: 1000, style: .continuous)
+                    .fill(Color.primary.opacity(0.20))
+                    .frame(width: 109, height: 109)
+                    .opacity(0.45)
+                    .glassEffect()
+
+                Image(systemName: "flame.fill")
+                    .font(.system(size: 36, weight: .bold))
+                    .foregroundStyle(Color(red: 255/255, green: 146/255, blue: 48/255, opacity: 1.0))
+                    .frame(width: 67, height: 43, alignment: .center)
+
+
+            }
+
         }
         .padding()
-        .glassEffect()
     }
 }
 
