fc3e591451b36932f5a597c68d59978814d77f3a
Suhaylah <suhailahawsawi@gmail.com>
2025-10-18 15:41:22 +0300

    full on boarding page

---
+++ Learning Tracking App/onBoardingPage.swift
@@ -0,0 +1,123 @@
+//
+//  ContentView.swift
+//  Learning Tracking App
+//
+//  Created by Suhaylah hawsawi on 24/04/1447 AH.
+//
+
+import SwiftUI
+
+struct onBoardingPage: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 24) {
+            // Logo
+            ZStack {
+                RoundedRectangle(cornerRadius: 1000)
+                    .fill(Color.primary.opacity(0.2))
+                    .frame(width: 109, height: 109)
+                    .glassEffect()
+
+                Image(systemName: "flame.fill")
+                    .font(.system(size: 36, weight: .bold))
+                    .foregroundStyle(Color.richOrange)
+            }
+            .frame(maxWidth: .infinity, alignment: .center)
+
+            // Greeting
+            VStack(alignment: .leading, spacing: 4) {
+                Text("Hello Learner")
+                    .font(.system(size: 34, weight: .bold))
+
+                Text("This app will help you learn everyday!")
+                    .font(.system(size: 17))
+                    .foregroundColor(Color(red: 153/255, green: 153/255, blue: 153/255))
+            }
+
+            // Input Section
+            VStack(alignment: .leading, spacing: 4) {
+                Text("I want to learn")
+                    .font(.system(size: 22))
+
+                TextField("Swift", text: .constant(""))
+                    .textFieldStyle(.plain)
+                Divider()
+
+
+            }
+            
+            VStack(alignment:.leading, spacing: 12){
+                Text("I want to learn it in a")
+                    .font(.system(size: 22))
+
+                TimeFilterView()
+            }
+
+            // Spacer to push button down
+            Spacer()
+
+            // Start Button
+            StartLearningButton {
+                print("Start Learning tapped!")
+            }
+            .frame(maxWidth: .infinity, alignment: .center)
+        }
+        .padding(.horizontal, 24)
+        .padding(.vertical, 32)
+    }
+}
+
+
+
+struct TimeFilterView: View {
+    @State private var selected = "Week"
+
+    var body: some View {
+        HStack(spacing: 8) {
+            button(title: "Week")
+            button(title: "Month")
+            button(title: "Year")
+        }
+    }
+
+    private func button(title: String) -> some View {
+        Button(action: {
+            selected = title
+        }) {
+            Text(title)
+                .font(.system(size: 17, weight: .medium))
+                .foregroundColor(.white)
+                .frame(width: 97, height: 48)
+                .glassEffect()
+                .background(
+                    RoundedRectangle(cornerRadius: 40)
+                        .fill(selected == title ? Color.orange : Color.black.opacity(0.1))
+                       
+                        .glassEffect()
+                )
+        }
+    }
+}
+
+
+struct StartLearningButton: View {
+    var action: () -> Void
+
+    var body: some View {
+        Button(action: action) {
+            Text("Start Learning")
+                .font(.system(size: 17, weight: .medium))
+                .foregroundColor(.white)
+                .frame(width: 182, height: 48)
+                .glassEffect()
+                .background(
+                    RoundedRectangle(cornerRadius: 40)
+                        .fill(Color.richOrange)
+                )
+        }
+    }
+}
+
+
+#Preview {
+    onBoardingPage()
+}
